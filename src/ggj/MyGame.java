67c767df06fae4b971fab6ad46265b1b382455c7
libjared <libjared@users.noreply.github.com>
2016-01-29 20:16:38 -0500

    initial commit!

---
+++ src/ggj/MyGame.java
@@ -0,0 +1,25 @@
+package ggj;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class MyGame extends BasicGame
+{
+	public MyGame(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 50, 50);
+	}
+}
---
+++ src/ggj/Program.java
@@ -0,0 +1,22 @@
+package ggj;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+public class Program {
+    public static void main(String[] args) {
+        try
+        {
+            AppGameContainer appgc;
+            appgc = new AppGameContainer(new MyGame("Simple Slick Game"));
+            appgc.setDisplayMode(640, 480, false);
+            appgc.start();
+        }
+        catch (SlickException ex)
+        {
+            Logger.getLogger(MyGame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
