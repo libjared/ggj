1b327da8248ec1a548113a3b0863aafde4dbf2a2
libjared <libjared@users.noreply.github.com>
2016-01-29 20:20:55 -0500

    clean code

--- src/ggj/MyGame.java
+++ src/ggj/MyGame.java
@@ -6,20 +6,20 @@ import org.newdawn.slick.SlickException;
 
 public class MyGame extends BasicGame
 {
-	public MyGame(String gamename)
-	{
-		super(gamename);
-	}
+    public MyGame(String gamename)
+    {
+        super(gamename);
+    }
 
-	@Override
-	public void init(GameContainer gc) throws SlickException {}
+    @Override
+    public void init(GameContainer gc) throws SlickException {}
 
-	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
+    @Override
+    public void update(GameContainer gc, int i) throws SlickException {}
 
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
-		g.drawString("Howdy!", 50, 50);
-	}
+    @Override
+    public void render(GameContainer gc, Graphics g) throws SlickException
+    {
+        g.drawString("Howdy!", 50, 50);
+    }
 }
