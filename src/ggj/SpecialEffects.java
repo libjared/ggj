52c5dd3d4604812fe5eb0e4526ebe0b922dfc47c
libjared <libjared@users.noreply.github.com>
2016-01-31 07:17:01 -0500

    wow\1! even bigger monsters!

--- src/ggj/SpecialEffects.java
+++ src/ggj/SpecialEffects.java
@@ -123,8 +123,8 @@ class CrashFx {
 
 class SummonFx {
     Image img;
-    float scale = 0.5f;
-    final float MAXSCALE = 1.3f;
+    float scale = 0.6f;
+    final float MAXSCALE = 1.8f;
     int summonTimer = 120;
     String ticker;
 }
