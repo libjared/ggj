3c05ad4abf5f0381ae2fc252e31033306979ee52
Jotham Callaway <Jotham Callaway@Jotham>
2016-02-18 02:16:09 -0500

    skeleton pt.1

---
+++ src/ggj/Gems.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggj;
+
+/**
+ *
+ * @author Jotham Callaway
+ */
+
+
+public class Gems {
+    
+    
+    private GemType color;
+    
+    public Gems(GemType color){
+        this.color = color;
+    }
+    
+    public GemType getColor(){
+        return color;
+    }
+}
