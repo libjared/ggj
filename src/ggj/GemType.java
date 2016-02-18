d92f2d131af00d66196937dabcb2955a3671d444
Jotham Callaway <Jotham Callaway@Jotham>
2016-02-18 02:17:00 -0500

    skeleton pt.3 (Eggplant)

---
+++ src/ggj/GemType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ggj;
+
+/**
+ *
+ * @author Jotham Callaway
+ */
+public enum GemType {
+    EMPTY, RED, GREEN, BLUE, YELLOW, PURPLE
+}
