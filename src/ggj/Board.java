3dd8cf31b9d69a6df0d72cb4eb87a91095518bf2
Jotham Callaway <Jotham Callaway@Jotham>
2016-02-17 22:19:16 -0500

    Red & Blue

--- src/ggj/Board.java
+++ src/ggj/Board.java
@@ -14,6 +14,7 @@ public class Board {
     public static final int WIDTH = 8;
     public static final int HEIGHT = 16;
     Random rng = new Random();
+    public static int redCall = 0;
 
     //none,R,G,B,Y,P is 0 to 5
     int[][] spaces;
@@ -23,19 +24,19 @@ public class Board {
     int[] fallingGems;
 
     ArrayList<int[]> markedForDeath;
-    
+
     final int KILLSTOSUMMON = 20;
     int kills = 0;
     int summonColor = 0;
-    
+
     public boolean PLAYERONE;
-    
+
     int hasteTimer = 0;
-    final int HASTETIMERMAX = 7*60; //7s
+    final int HASTETIMERMAX = 7 * 60; //7s
 
     public Board(boolean playerOne) throws SlickException {
         PLAYERONE = playerOne;
-        
+
         this.markedForDeath = new ArrayList<>();
         spaces = new int[HEIGHT][WIDTH];
         rngBuf = new ArrayList<>();
@@ -60,14 +61,14 @@ public class Board {
     private boolean kShufLast;
     private boolean kSummonLast;
     private boolean kDrainLast;
-    
+
     private boolean kDown;
     private boolean kLeft;
     private boolean kRight;
     private boolean kShuf;
     private boolean kSummon;
     private boolean kDrain;
-    
+
     public void update(GameContainer gc) {
         updateInputs(gc);
 
@@ -76,16 +77,16 @@ public class Board {
         findMatches();
 
         processMarkedForDeath();
-        
+
         updateGravity();
-        
+
         checkForDeath();
-        
+
         updateSummon();
     }
-    
+
     private void checkForDeath() {
-        if (gemExistsAt(WIDTH/2, 0) && !doesNeedGravity()) {
+        if (gemExistsAt(WIDTH / 2, 0) && !doesNeedGravity()) {
             MyGame.winner = getOtherBoard();
             ContentContainer.getGong().play();
         }
@@ -95,12 +96,12 @@ public class Board {
         if (kills >= KILLSTOSUMMON && kSummon && !kSummonLast) {
             //do the summon
             applySummon();
-            
+
             //then reset
             kills = 0;
             summonColor = 0;
         }
-        
+
         //if you want your summon meter reset
         if (kills > 0 && kDrain && !kDrainLast) {
             kills = 0;
@@ -108,13 +109,13 @@ public class Board {
             ContentContainer.getVanish().play();
         }
     }
-    
+
     private void applySummon() {
         //012345
         //_rgbyp
-        
+
         ContentContainer.summonSoundFromColor(summonColor).play();
-        
+
         switch (summonColor) {
             case 0:
             default:
@@ -135,7 +136,7 @@ public class Board {
                 getOtherBoard().doPurpleMagic();
                 break;
         }
-        
+
         SpecialEffects.setSummon(summonColor);
     }
 
@@ -144,7 +145,7 @@ public class Board {
         remakeBoardWithHeight(averageHeight);
         getOtherBoard().remakeBoardWithHeight(averageHeight);
     }
-    
+
     private void remakeBoardWithHeight(int h) {
         for (int y = h; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
@@ -152,7 +153,7 @@ public class Board {
             }
         }
     }
-    
+
     private int findHeight() {
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
@@ -163,46 +164,93 @@ public class Board {
         }
         return HEIGHT;
     }
-    
+
     private void doPurpleMagic() {
         hasteTimer = HASTETIMERMAX;
     }
-    
+
     private void doBlueMagic() {
         //shift all to right
         //int[][] newSpaces = cloneSpaces();
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = WIDTH - 1; x >= 0; x--) {
-                int colorThere;
-                if (isValid(x - 1, y)) {
-                    colorThere = getSpace(y, x - 1);
-                } else {
-                    colorThere = 0;
-                }
-                setSpace(y, x, colorThere);
+        /*for (int y = 0; y < HEIGHT; y++) {
+         for (int x = WIDTH - 1; x >= 0; x--) {
+         int colorThere;
+         if (isValid(x - 1, y)) {
+         colorThere = getSpace(y, x - 1);
+         } else {
+         colorThere = 0;
+         }
+         setSpace(y, x, colorThere);
+         }
+         }*/
+        // destroys gems in three diagonal lines (no duplicates)
+
+        boolean duplicatesCheck = true;
+
+        int diagonalS1 = rng.nextInt(9) + 1;
+        int diagonalS2 = rng.nextInt(9) + 1;
+        int diagonalS3 = rng.nextInt(9) + 1;
+
+        do {
+            if (diagonalS1 == diagonalS2) {
+                diagonalS2 = rng.nextInt(9) + 1;
+                continue;
             }
+            if (diagonalS2 == diagonalS3) {
+                diagonalS3 = rng.nextInt(9) + 1;
+                continue;
+            }
+            if (diagonalS3 == diagonalS1) {
+                diagonalS1 = rng.nextInt(9) + 1;
+                continue;
+            }
+            duplicatesCheck = false;
+        } while (duplicatesCheck);
+
+        for (int i = 8; i > 0; i--) {
+            destroyGem(i, diagonalS1);
+            destroyGem(i, diagonalS2);
+            destroyGem(i, diagonalS3);
+
+            diagonalS1--;
+            diagonalS2--;
+            diagonalS3--;
         }
     }
-    
+
     private void doGreenMagic() {
         //color snipe
+        //cross Atack in progress
+        int crossAttack = rng.nextInt(10);
         int biggestColor = getBiggestColor();
-        
+
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
                 int colorHere = getSpace(y, x);
                 if (colorHere == biggestColor) {
-                    markedForDeath.add(new int[] { x, y });
+                    markedForDeath.add(new int[]{x, y});
                 }
             }
         }
     }
-    
+
     private void doRedMagic() {
         //spawn junk on the top row
-        for (int x = 0; x < WIDTH; x++) {
-            setSpace(0, x, randomColor());
+        redCall++;
+        int redLevels = 0;
+        if (redCall > 4) {
+            redLevels = 1;
+            if (redCall > 8) {
+                redLevels = 2;
+            }
         }
+        for (int i = 0; i < redLevels; i++) {
+            //the orginal code is here.
+            for (int x = 0; x < WIDTH; x++) {
+                setSpace(i, x, randomColor());
+            }
+        }
+
     }
 
     private int getBiggestColor() {
@@ -225,10 +273,11 @@ public class Board {
         }
         return biggestColor;
     }
-    
+
     private Board getOtherBoard() {
-        if (PLAYERONE)
+        if (PLAYERONE) {
             return MyGame.right;
+        }
         return MyGame.left;
     }
 
@@ -238,7 +287,7 @@ public class Board {
         } else {
             gravTimer = 0;
         }
-        
+
         if (gravTimer >= GRAVTIMERMAX) {
             gravTimer = 0;
             applyGravity();
@@ -250,15 +299,16 @@ public class Board {
             fallColumn(x);
         }
     }
-    
+
     final int GRAVTIMERMAX = 15;
     int gravTimer = 0;
-    
+
     private boolean doesNeedGravity() {
         for (int x = 0; x < WIDTH; x++) {
             ArrayList<Integer> thisCol = columnToArrayList(x);
-            if (columnNeedsGravity(thisCol))
+            if (columnNeedsGravity(thisCol)) {
                 return true;
+            }
         }
         return false;
     }
@@ -270,7 +320,7 @@ public class Board {
         kShufLast = kShuf;
         kSummonLast = kSummon;
         kDrainLast = kDrain;
-        
+
         Input inp = gc.getInput();
         if (PLAYERONE) {
             kDown = inp.isKeyDown(Input.KEY_DOWN);
@@ -291,10 +341,10 @@ public class Board {
 
     final int SHUFFLEANIMMAX = 10;
     int shuffleAnim = 0;
-    
+
     private void updateFallingGems() {
         shuffleAnim = Math.max(shuffleAnim - 1, 0);
-        
+
         //shuffle
         if (kShuf && !kShufLast) {
             int[] newGems = new int[3];
@@ -304,7 +354,7 @@ public class Board {
             fallingGems = newGems;
             shuffleAnim = SHUFFLEANIMMAX;
         }
-        
+
         if (!doesNeedGravity()) {
             updateFallingGemsPos();
             collideFallingGems();
@@ -320,19 +370,19 @@ public class Board {
             fallSpd = 5f / 60f;
         }
         fallingGemY += fallSpd;
-        
+
         //update x
-        int realBottomY = (int)(fallingGemY+3);
-        if (kLeft && !kLeftLast && isValid(fallingGemX-1, realBottomY)
-                && !gemExistsAt(fallingGemX-1, realBottomY)) {
+        int realBottomY = (int) (fallingGemY + 3);
+        if (kLeft && !kLeftLast && isValid(fallingGemX - 1, realBottomY)
+                && !gemExistsAt(fallingGemX - 1, realBottomY)) {
             fallingGemX--;
         }
-        if (kRight && !kRightLast && isValid(fallingGemX+1, realBottomY)
-                && !gemExistsAt(fallingGemX+1, realBottomY)) {
+        if (kRight && !kRightLast && isValid(fallingGemX + 1, realBottomY)
+                && !gemExistsAt(fallingGemX + 1, realBottomY)) {
             fallingGemX++;
         }
     }
-    
+
     private void findMatches() {
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
@@ -378,10 +428,11 @@ public class Board {
     private void destroyGem(int x, int y) {
         //check if already marked
         for (int[] ded : markedForDeath) {
-            if (ded[0] == x && ded[1] == y)
+            if (ded[0] == x && ded[1] == y) {
                 return;
+            }
         }
-        
+
         markedForDeath.add(new int[]{x, y});
     }
 
@@ -390,13 +441,15 @@ public class Board {
      */
     private boolean fallColumn(int x) {
         boolean didFall = false;
-        
+
         ArrayList<Integer> newCol = columnToArrayList(x);
-        
+
         boolean needGravity = columnNeedsGravity(newCol);
-        
-        if (!needGravity) return false;
-        
+
+        if (!needGravity) {
+            return false;
+        }
+
         //delete the first 0 from the bottom
         for (int i = 0; i < newCol.size(); i++) {
             if (newCol.get(i) == 0) {
@@ -405,18 +458,18 @@ public class Board {
                 break;
             }
         }
-        
+
         //pad with 0s to get HEIGHT-1 elements
         while (newCol.size() != HEIGHT) {
             newCol.add(0);
         }
-        
+
         //copy back to column
         for (int i = 0; i < HEIGHT; i++) {
             int destY = HEIGHT - i - 1;
-            setSpace(destY,x, newCol.get(i));
+            setSpace(destY, x, newCol.get(i));
         }
-        
+
         return didFall;
     }
 
@@ -424,7 +477,7 @@ public class Board {
         //convert column to arraylist
         ArrayList<Integer> newCol = new ArrayList<>();
         for (int y = HEIGHT - 1; y >= 0; y--) {
-            newCol.add(getSpace(y,x));
+            newCol.add(getSpace(y, x));
         }
         return newCol;
     }
@@ -444,22 +497,25 @@ public class Board {
         }
         return needGravity;
     }
-    
+
     private void processMarkedForDeath() {
-        if (markedForDeath.size() > 0)
-            ContentContainer.getCrash().play(1f + (float)Math.random() * 0.3f, 1f);
-        
+        if (markedForDeath.size() > 0) {
+            ContentContainer.getCrash().play(1f + (float) Math.random() * 0.3f, 1f);
+        }
+
         for (int i = 0; i < markedForDeath.size(); i++) {
             int[] gem = markedForDeath.get(i);
             int theX = gem[0];
             int theY = gem[1];
-            int theColor = getSpace(theY,theX);
-            
+            int theColor = getSpace(theY, theX);
+
             if (summonColor == 0) //first hit, set summon color
+            {
                 summonColor = theColor;
-            
+            }
+
             SpecialEffects.addCrash(theX, theY, offsetx, offsety, theColor);
-            
+
             setSpace(theY, theX, 0);
         }
         kills += markedForDeath.size();
@@ -493,51 +549,51 @@ public class Board {
 
     int offsetx = 0;
     int offsety = 0;
-    
+
     public void draw(Graphics g, int offsetx, int offsety) throws SlickException {
         this.offsetx = offsetx;
         this.offsety = offsety;
-        
+
         drawBoardGems(g);
 
         drawFallingGems(g);
-        
+
         drawNextGems(g);
-        
+
         drawSummonBars(g);
     }
 
     private void drawSummonBars(Graphics g) {
         Color pushCol = g.getColor();
         float pushLW = g.getLineWidth();
-        
+
         g.setLineWidth(2f);
-        
+
         //outline        
         g.setColor(Color.blue);
-        g.drawRect(this.offsetx, HEIGHT*32 + 48f, WIDTH*32, 32);
-        
+        g.drawRect(this.offsetx, HEIGHT * 32 + 48f, WIDTH * 32, 32);
+
         float colScalar = 1f;
-        
+
         if (kills != 0) {
-            float ratio = kills / (float)KILLSTOSUMMON;
+            float ratio = kills / (float) KILLSTOSUMMON;
             if (ratio >= 1f) {
                 ratio = 1f;
                 wooshyWooshColorThing += 0.1f;
-                colScalar = (float) (Math.cos(wooshyWooshColorThing)+1)/2;
+                colScalar = (float) (Math.cos(wooshyWooshColorThing) + 1) / 2;
             } else {
                 wooshyWooshColorThing = 0f;
             }
             Color newCol = colorFromInt(summonColor).scaleCopy(colScalar);
             newCol.a = 1f;
             g.setColor(newCol);
-            g.fillRect(this.offsetx, HEIGHT*32 + 48f, ratio*WIDTH*32, 32);
+            g.fillRect(this.offsetx, HEIGHT * 32 + 48f, ratio * WIDTH * 32, 32);
         }
-        
+
         g.setLineWidth(pushLW);
         g.setColor(pushCol);
     }
-    
+
     private float wooshyWooshColorThing = 0f;
 
     private static Color colorFromInt(int i) {
@@ -576,10 +632,10 @@ public class Board {
             drawGem(g, fallingGems[1], fallingGemX, fallingGemY + 1);
             drawGem(g, fallingGems[2], fallingGemX, fallingGemY + 2);
         } else {
-            float ratio = shuffleAnim / (float)SHUFFLEANIMMAX;
+            float ratio = shuffleAnim / (float) SHUFFLEANIMMAX;
             int finalX = fallingGemX * 32 + offsetx;
             float finalY;
-            
+
             //first (from third)
             int color1 = fallingGems[0];
             finalY = fallingGemY * 32 + offsety;
@@ -587,8 +643,8 @@ public class Board {
             g.drawImage(imageCol(color1),
                     finalX, finalY,
                     finalX + 32, finalBottomY,
-                    0, 64*ratio, 64, 64);
-            
+                    0, 64 * ratio, 64, 64);
+
             //second (from first)
             int color2 = fallingGems[1];
             finalY = (fallingGemY + 1 - ratio) * 32 + offsety;
@@ -597,7 +653,7 @@ public class Board {
                     finalX, finalY,
                     finalX + 32, finalBottomY,
                     0, 0, 64, 64);
-            
+
             //third (from second)
             int color3 = fallingGems[2];
             finalY = (fallingGemY + 2 - ratio) * 32 + offsety;
@@ -606,18 +662,18 @@ public class Board {
                     finalX, finalY,
                     finalX + 32, finalBottomY,
                     0, 0, 64, 64);
-            
+
             //fourth! (from third)
             finalY = (fallingGemY + 3 - ratio) * 32 + offsety;
             finalBottomY = (fallingGemY + 3) * 32 + offsety;
             g.drawImage(imageCol(color1),
                     finalX, finalY,
                     finalX + 32, finalBottomY,
-                    0, 0, 64, ratio*64);
+                    0, 0, 64, ratio * 64);
         }
     }
-    
-    private void drawNextGems(Graphics g){
+
+    private void drawNextGems(Graphics g) {
         int nextX;
         int nextY = 55;
         if (PLAYERONE) {
@@ -625,9 +681,9 @@ public class Board {
         } else {
             nextX = 458;
         }
-        
+
         int[] nextThree = peekNextThree();
-        
+
         for (int i = 0; i < nextThree.length; i++) {
             g.drawImage(imageCol(nextThree[i]),
                     nextX, nextY + i * 32,
@@ -654,14 +710,17 @@ public class Board {
         boolean collideBottom = fallingGemY > HEIGHT;
 
         if (collideGem || collideBottom) {
-            if (isValid(hitX, hitY - 1))
-                setSpace(hitY - 1,fallingGemX,fallingGems[2]);
-            if (isValid(hitX, hitY - 2))
-                setSpace(hitY - 2,fallingGemX,fallingGems[1]);
-            if (isValid(hitX, hitY - 3))
-                setSpace(hitY - 3,fallingGemX,fallingGems[0]);
-            
-            ContentContainer.getDink().play(1f + (float)Math.random() * 0.3f, 1f);
+            if (isValid(hitX, hitY - 1)) {
+                setSpace(hitY - 1, fallingGemX, fallingGems[2]);
+            }
+            if (isValid(hitX, hitY - 2)) {
+                setSpace(hitY - 2, fallingGemX, fallingGems[1]);
+            }
+            if (isValid(hitX, hitY - 3)) {
+                setSpace(hitY - 3, fallingGemX, fallingGems[0]);
+            }
+
+            ContentContainer.getDink().play(1f + (float) Math.random() * 0.3f, 1f);
 
             generateFallingGems();
 
@@ -677,8 +736,9 @@ public class Board {
     private Image imageCol(int col) {
         return ContentContainer.imageFromColor(col);
     }
-    
+
     ArrayList<Integer> rngBuf;
+
     private int randomColor() {
         int newRand = useRngToMakeANewColor();
         rngBuf.add(newRand);
@@ -688,21 +748,21 @@ public class Board {
     private int useRngToMakeANewColor() {
         return rng.nextInt(5) + 1;
     }
-    
+
     private void ensureBuffer(int n) {
         while (rngBuf.size() < n) {
             rngBuf.add(useRngToMakeANewColor());
         }
     }
-    
+
     public int[] peekNextThree() {
-        return new int[] { rngBuf.get(0), rngBuf.get(1), rngBuf.get(2) };
+        return new int[]{rngBuf.get(0), rngBuf.get(1), rngBuf.get(2)};
     }
-    
+
     int getSpace(int y, int x) {
         return spaces[y][x];
     }
-    
+
     void setSpace(int y, int x, int col) {
         spaces[y][x] = col;
     }
