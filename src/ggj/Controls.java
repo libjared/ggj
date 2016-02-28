7455a32e4b4901ca406865cd6c836023c41b381b
libjared <libjared@users.noreply.github.com>
2016-02-27 19:39:16 -0500

    separate into autocontrollerselector

--- src/ggj/Controls.java
+++ src/ggj/Controls.java
@@ -1,15 +1,11 @@
 package ggj;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.newdawn.slick.ControllerListener;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Input;
 
 class Controls {
 
-    static GameContainer gameContainer;
-    static MyControllerListener myControllerListener;
+    static AutoControllerSelector autoCont;
     static Input gcInput;
 
     final private static int SNES_B = 2; //18 on mac
@@ -21,109 +17,35 @@ class Controls {
     final private static int CONTROLLERBUTTONDRAIN = SNES_X;
 
     static void start(GameContainer gc) {
-        gameContainer = gc;
-        myControllerListener = new MyControllerListener();
-        gcInput = gameContainer.getInput();
-        gcInput.addControllerListener(myControllerListener);
+        autoCont = new AutoControllerSelector();
+        gcInput = gc.getInput();
+        gcInput.addControllerListener(autoCont);
     }
 
     static boolean getDown() {
-        return gcInput.isControllerDown(myControllerListener.getLastControllerToGetUsed());
+        return gcInput.isControllerDown(autoCont.getLastControllerToGetUsed());
     }
 
     static boolean getLeft() {
-        return gcInput.isControllerLeft(myControllerListener.getLastControllerToGetUsed());
+        return gcInput.isControllerLeft(autoCont.getLastControllerToGetUsed());
     }
 
     static boolean getRight() {
-        return gcInput.isControllerRight(myControllerListener.getLastControllerToGetUsed());
+        return gcInput.isControllerRight(autoCont.getLastControllerToGetUsed());
     }
 
     static boolean getShuf() {
         return gcInput.isButtonPressed(CONTROLLERBUTTONSHUFFLE,
-                myControllerListener.getLastControllerToGetUsed());
+                autoCont.getLastControllerToGetUsed());
     }
 
     static boolean getSummon() {
         return gcInput.isButtonPressed(CONTROLLERBUTTONSUMMON,
-                myControllerListener.getLastControllerToGetUsed());
+                autoCont.getLastControllerToGetUsed());
     }
 
     static boolean getDrain() {
         return gcInput.isButtonPressed(CONTROLLERBUTTONDRAIN,
-                myControllerListener.getLastControllerToGetUsed());
-    }
-}
-
-class MyControllerListener implements ControllerListener {
-
-    private int lastControllerToGetUsed = 0;
-
-    public int getLastControllerToGetUsed() {
-        return lastControllerToGetUsed;
-    }
-
-    @Override
-    public void controllerLeftPressed(int controllerId) {
-        lastControllerToGetUsed = controllerId;
-    }
-
-    @Override
-    public void controllerRightPressed(int controllerId) {
-        lastControllerToGetUsed = controllerId;
-    }
-
-    @Override
-    public void controllerUpPressed(int controllerId) {
-        lastControllerToGetUsed = controllerId;
-    }
-
-    @Override
-    public void controllerDownPressed(int controllerId) {
-        lastControllerToGetUsed = controllerId;
-    }
-
-    @Override
-    public void controllerButtonPressed(int controllerId, int buttonCode) {
-        lastControllerToGetUsed = controllerId;
-        Logger.getLogger(MyControllerListener.class.getName()).log(Level.WARNING, String.format("Controller#%d pressed button with code %d", controllerId, buttonCode - 1));
-    }
-
-    @Override
-    public void setInput(Input input) {
-        Logger.getLogger(MyControllerListener.class.getName()).log(Level.WARNING, "Set input!?");
-    }
-
-    @Override
-    public boolean isAcceptingInput() {
-        return true;
-    }
-
-    @Override
-    public void inputStarted() {
-    }
-
-    @Override
-    public void inputEnded() {
-    }
-
-    @Override
-    public void controllerButtonReleased(int i, int i1) {
-    }
-
-    @Override
-    public void controllerUpReleased(int i) {
-    }
-
-    @Override
-    public void controllerDownReleased(int i) {
-    }
-
-    @Override
-    public void controllerLeftReleased(int i) {
-    }
-
-    @Override
-    public void controllerRightReleased(int i) {
+                autoCont.getLastControllerToGetUsed());
     }
 }
