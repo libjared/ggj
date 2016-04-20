1eec4ea3e7c8f65fc41932ab5bc779d8b10075f5
Jotham Callaway <Jotham Callaway@Jotham>
2016-04-20 02:09:46 -0400

    Moved to gameLogic package

---
+++ src/ggj/gameLogic/Gem.java
@@ -0,0 +1,20 @@
+package ggj.gameLogic;
+
+import ggj.gameLogic.GemType;
+
+/**
+ *
+ * @author Jotham Callaway
+ */
+public class Gem {
+
+    private final GemType color;
+
+    public Gem(GemType color) {
+        this.color = color;
+    }
+
+    public GemType getColor() {
+        return color;
+    }
+}
